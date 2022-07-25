093ef992da37d1416a0417d33997b88907486351
Zubair Niazi <zubair@coral.global>
2022-07-25 11:22:49 -0700

    setup mongo

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workout');
 
 // express app
@@ -15,7 +16,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port', process.env.PORT);
-});
+// connect mongoose
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to DB & listening on port:', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log('Error connecting to Mongo:', error.message);
+  });
