e6f38bc6e196e41ba04cf2469766fca7474876c7
Zubair Niazi <zubair@coral.global>
2022-07-25 10:50:02 -0700

    setup app

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+
+// express app
+const app = express();
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to express app' });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port', process.env.PORT);
+});
